5dfdffccdd475f11c9e46e9fdea22d2eb320adc9
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2021-10-21 21:55:29 -0300

    Consumes GitHub API

---
+++ script.js
@@ -0,0 +1,35 @@
+const socialMediaLinks = {
+  github: "thiagobrito-dev",
+  youtube: "rocketseat",
+  instagram: "rocketseat_oficial",
+  facebook: "rocketseat",
+  twitter: "rocketseat",
+};
+
+function changeSocialMediaLinks() {
+  const listItems = socialLinks.children;
+
+  for (let item of listItems) {
+    const socialMedia = item.getAttribute("class");
+    const link = item.children[0];
+
+    link.href = `https://${socialMedia}.com/${socialMediaLinks[socialMedia]}`;
+  }
+}
+
+function getUserGitHubProfileInfo() {
+  const url = `https://api.github.com/users/${socialMediaLinks.github}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      userImage.src = data.avatar_url;
+      userName.textContent = data.name;
+      profileLink.href = data.html_url;
+      gitHubUser.textContent = data.login;
+      userBiography.textContent = data.bio;
+    });
+}
+
+changeSocialMediaLinks();
+getUserGitHubProfileInfo();
