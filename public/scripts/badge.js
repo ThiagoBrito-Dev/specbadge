ef4796e17fa16a02c921fa8c88395821ddbe8294
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2021-11-18 20:48:57 -0300

    Stores and displays the edited data

--- public/scripts/badge.js
+++ public/scripts/badge.js
@@ -1,35 +1,73 @@
-const socialMediaLinks = {
-  github: "trybrito",
-  youtube: "rocketseat",
-  instagram: "trybrito",
-  facebook: "rocketseat",
-  twitter: "trybrito",
-};
+function toggleModalState() {
+  modalOverlay.classList.toggle("active");
+}
 
-function changeSocialMediaLinks() {
-  const listItems = socialLinks.children;
+function updateBadgeInfo(event) {
+  event.preventDefault();
 
-  for (let item of listItems) {
-    const socialMedia = item.getAttribute("class");
-    const link = item.children[0];
+  const inputs = formEdit.querySelectorAll(".input");
+  const containers = configWrapper.querySelectorAll(".social-link-container");
+  const values = [];
 
-    link.href = `https://${socialMedia}.com/${socialMediaLinks[socialMedia]}`;
+  for (let index = 0; index < inputs.length; index++) {
+    const { value } = inputs[index];
+    values.push(value);
+
+    if (index === 0) {
+      const pageTitle = document.querySelector("head title");
+      pageTitle.textContent += ` - ${value}`;
+      continue;
+    }
+
+    const container = containers[index - 1];
+    const socialMedia = container.getAttribute("data-media");
+    const link = container.children[0];
+
+    link.href = `https://${socialMedia}.com/${value}`;
+    link.target = "_blank";
+  }
+
+  const badgeInfo = {
+    title: values[0],
+    youtube: values[1],
+    instagram: values[2],
+    facebook: values[3],
+    twitter: values[4],
+  };
+  localStorage.setItem("specbadge:badgeInfo", JSON.stringify(badgeInfo));
+}
+
+function getBadgeInfo(badgeInfo) {
+  badgeInfo = JSON.parse(badgeInfo);
+
+  const pageTitle = document.querySelector("head title");
+  pageTitle.textContent += ` - ${badgeInfo.title}`;
+
+  const inputs = formEdit.querySelectorAll(".input");
+  const containers = configWrapper.querySelectorAll(".social-link-container");
+
+  for (let index = 0; index < inputs.length; index++) {
+    const input = inputs[index];
+
+    if (index === 0) {
+      input.value = badgeInfo.title;
+      continue;
+    }
+
+    const container = containers[index - 1];
+    const socialMedia = container.getAttribute("data-media");
+    const link = container.children[0];
+
+    input.value = badgeInfo[socialMedia];
+    link.href = `https://${socialMedia}.com/${badgeInfo[socialMedia]}`;
+    link.target = "_blank";
   }
 }
 
-function getUserGitHubProfileInfo() {
-  const url = `https://api.github.com/users/${socialMediaLinks.github}`;
+const badgeInfo = localStorage.getItem("specbadge:badgeInfo");
+badgeInfo && getBadgeInfo(badgeInfo);
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      userImage.src = data.avatar_url;
-      userName.textContent = data.name;
-      profileLink.href = data.html_url;
-      gitHubUser.textContent = data.login;
-      userBiography.textContent = data.bio;
-    });
-}
-
-// changeSocialMediaLinks();
-// getUserGitHubProfileInfo();
+editBadge.addEventListener("click", toggleModalState);
+cancelButton.addEventListener("click", toggleModalState);
+saveButton.addEventListener("click", toggleModalState);
+formEdit.addEventListener("submit", updateBadgeInfo);
